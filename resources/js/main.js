695fca4c67de37203d600d85529bf8fbb2d4456c
Jamie <elliott1234@users.noreply.github.com>
2018-03-27 09:26:01 +0100

    final commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,5 @@
 var Trackster = {};
+const API_KEY = '878e1924043b6658c04d116ceb538843';
 
 $(document).ready(function() {
   $('#search-button').click(function() {
@@ -6,18 +7,36 @@ $(document).ready(function() {
   });
 });
 
-Main.returnsTrue = function() {
-  return false;
-};
+Trackster.renderTracks = function(tracks) {
+  var $trackList = $('#track-list');
 
-Main.returnsHello = function() {
-  return 'Goodbye';
-};
+  $trackList.empty();
 
-Main.returnsTheMeaningOfLife = function(number) {
-  if (number === 41) {
-    return 'The meaning of life is clear';
-  } else {
-    return 'The meaning of life is unclear';
+  for (var trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
+    var track = tracks[trackIndex];
+    var mediumAlbumArt = track.image[1]["#text"];
+    var htmlTrackRow =
+      '<div class="row track">' +
+      '  <div class="col-xs-1 col-xs-offset-1 play-button">' +
+      '    <a href="'+ track.url + '" target="_blank">' +
+      '      <i class="fa fa-play-circle-o fa-2x"></i>' +
+      '    </a>' +
+      '  </div>' +
+      '  <div class="col-xs-4">' + track.name + '</div>' +
+      '  <div class="col-xs-2">' + track.artist + '</div>' +
+      '  <div class="col-xs-2"><img src="' + mediumAlbumArt + '"/></div>' +
+      '  <div class="col-xs-2">' + track.listeners + '</div>' +
+      '</div>';
+
+    $trackList.append(htmlTrackRow);
   }
+};
+
+Trackster.searchTracksByTitle = function(title) {
+  $.ajax({
+    url: 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+    success: function(response) {
+      Trackster.renderTracks(response.results.trackmatches.track);
+    }
+  });
 };
